17b097e56dfdf65b44afd612e9d3dd621593747c
HyeongminKim <assa0620@gmail.com>
2023-03-15 12:54:08 +0900

    마이너 버그 수정

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class errorLevel(Enum):
 
 def check_terminal_output(command: str):
     try:
-        return subprocess.check_output(f'{command}', shell=True, encoding='utf-8').strip()
+        return subprocess.check_output(f'{command} 2> NUL', shell=True, encoding='utf-8').strip()
     except subprocess.CalledProcessError:
         logformat(errorLevel.ERR, f'{command} returned non-zero exit status.')
         return None
