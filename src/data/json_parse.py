8b34ee268ad58f50995c5b36ecdfbb81531bac55
HyeongminKim <assa0620@gmail.com>
2023-03-29 21:57:16 +0900

    명령행 인수를 처음 설정할 경우 예외처리

--- src/data/json_parse.py
+++ src/data/json_parse.py
@@ -23,13 +23,27 @@ def dumpConfigurationFile(external_cmd: str):
         with open(f'{userAppData}/Battle.net/Battle.net.config', 'r', encoding='utf-8') as file:
             data = load(file)
 
+        checkConfigurationStructure()
         data['Games']['osi']['AdditionalLaunchArguments'] = external_cmd
         logformat(errorLevel.INFO, f'Altered configured external command: "{external_cmd}".')
-
         with open(f'{userAppData}/Battle.net/Battle.net.config', 'w', encoding='utf-8') as file:
-            dump(data, file, indent='    ')
-
+            dump(data, file, indent=4)
         return True
     except (FileNotFoundError, KeyError):
         logformat(errorLevel.WARN, 'external command does not configured yet.')
         return False
+
+def checkConfigurationStructure():
+    try:
+        with open(f'{userAppData}/Battle.net/Battle.net.config', 'r', encoding='utf-8') as file:
+            data = load(file)
+
+        target = data['Games']['osi']['AdditionalLaunchArguments']
+        logformat(errorLevel.INFO, f'Tested configured external command: "{target}".')
+    except KeyError:
+        logformat(errorLevel.INFO, 'external command does not configured yet. Making configuration structures.')
+        data['Games']['osi']['AdditionalLaunchArguments'] = ''
+        with open(f'{userAppData}/Battle.net/Battle.net.config', 'w', encoding='utf-8') as file:
+            dump(data, file, indent=4)
+    except FileNotFoundError:
+        logformat(errorLevel.WARN, 'external command does not configured yet.')
