a0efcab6148b14a0bc166a08b25c9dc6e0089919
HyeongminKim <assa0620@gmail.com>
2023-03-31 18:37:51 +0900

    레지스트리 파싱기능 안정화

--- src/data/registry.py
+++ src/data/registry.py
@@ -9,25 +9,25 @@ def ReturnRegistryQuery(regAddress: str, queryName: str = 'InstallLocation'):
     target = None
     try:
         key = reg.HKEY_LOCAL_MACHINE
-        target = reg.OpenKey(key, regAddress, 0, reg.KEY_READ)
-        value, type = reg.QueryValueEx(target, queryName)
+        with reg.OpenKey(key, regAddress, 0, reg.KEY_READ) as target:
+            value, type = reg.QueryValueEx(target, queryName)
+
         if path.isfile(value) or path.isdir(value):
             logformat(errorLevel.INFO, f'{value} is exist in system.')
-            if(target is not None): reg.CloseKey(target)
             return value
         else:
             raise FileNotFoundError
     except (OSError, WindowsError, FileNotFoundError):
         logformat(errorLevel.ERR, f'Unable to locate {regAddress} registry.')
-        if(target is not None): reg.CloseKey(target)
         return None
 
 def OpenProgramUsingRegistry(regAddress: str, queryName: str = 'DisplayIcon'):
     target = None
     try:
         key = reg.HKEY_LOCAL_MACHINE
-        target = reg.OpenKey(key, regAddress, 0, reg.KEY_READ)
-        value, type = reg.QueryValueEx(target, queryName)
+        with reg.OpenKey(key, regAddress, 0, reg.KEY_READ) as target:
+            value, type = reg.QueryValueEx(target, queryName)
+
         if path.isfile(value) or path.isdir(value):
             logformat(errorLevel.INFO, f'{value} is exist in system.')
             startfile(value)
@@ -35,40 +35,36 @@ def OpenProgramUsingRegistry(regAddress: str, queryName: str = 'DisplayIcon'):
             logformat(errorLevel.ERR, 'Unable to launch target file or directory: no such file or directory.')
     except (OSError, WindowsError):
         logformat(errorLevel.ERR, f'Unable to locate {regAddress} registry.')
-    finally:
-        if(target is not None): reg.CloseKey(target)
 
 def TestRegistryValueAsFile(regAddress: str, queryName: str = 'DisplayIcon'):
     target = None
     try:
         key = reg.HKEY_LOCAL_MACHINE
-        target = reg.OpenKey(key, regAddress, 0, reg.KEY_READ)
-        value, type = reg.QueryValueEx(target, queryName)
+        with reg.OpenKey(key, regAddress, 0, reg.KEY_READ) as target:
+            value, type = reg.QueryValueEx(target, queryName)
+
         if path.isfile(value) or path.isdir(value):
             logformat(errorLevel.INFO, f'{value} is exist in system.')
-            if(target is not None): reg.CloseKey(target)
             return True
         else:
             raise FileNotFoundError
     except (OSError, WindowsError, FileNotFoundError):
         logformat(errorLevel.ERR, f'Unable to locate {regAddress} registry.')
-        if(target is not None): reg.CloseKey(target)
         return False
 
 def TestRegistryValueAsRaw(regAddress: str, queryName: str):
     target = None
     try:
         key = reg.HKEY_CURRENT_USER
-        target = reg.OpenKey(key, regAddress, 0, reg.KEY_READ)
-        value, type = reg.QueryValueEx(target, queryName)
+        with reg.OpenKey(key, regAddress, 0, reg.KEY_READ) as target:
+            value, type = reg.QueryValueEx(target, queryName)
+
         if value == 0:
             logformat(errorLevel.INFO, f'{value} is exist in system.')
-            if(target is not None): reg.CloseKey(target)
             return True
         else:
             raise OSError
     except (OSError, WindowsError):
         logformat(errorLevel.ERR, f'Unable to locate {regAddress} registry.')
-        if(target is not None): reg.CloseKey(target)
         return False
 
